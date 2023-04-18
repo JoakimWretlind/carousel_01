04df4ea558d2f71d01d9c2148615304102c927a3
JoakimWretlind <joakim.wretlind@gmail.com>
2023-04-19 00:09:50 +0200

    Set up for the coursel

---
+++ src/components/slider/index.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { Carousel } from "./Carousel";
+import "./style.scss";
+
+import pic01 from "/images/pic01.jpg";
+import pic02 from "/images/pic02.jpg";
+import pic03 from "/images/pic03.jpg";
+import pic04 from "/images/pic04.jpg";
+import pic05 from "/images/pic05.jpg";
+
+const slides = [
+  { id: 1, pic: pic01, title: "slide1" },
+  { id: 2, pic: pic02, title: "slide2" },
+  { id: 3, pic: pic03, title: "slide3" },
+  { id: 4, pic: pic04, title: "slide4" },
+  { id: 5, pic: pic05, title: "slide5" },
+];
+
+const Slider = () => {
+  const [index, setIndex] = useState<number>(0);
+  const lastIndex = slides.length - 1;
+
+  const prevSlide = () => {
+    setIndex(index - 1);
+    if (index < 1) {
+      setIndex(lastIndex);
+    }
+  };
+
+  const nextSlide = () => {
+    setIndex(index + 1);
+    if (index > lastIndex - 1) {
+      setIndex(0);
+    }
+  };
+  return (
+    <div className="pageWrapper">
+      <button className="directionBtn" onClick={prevSlide}>
+        &lt;
+      </button>
+      <Carousel slides={slides} index={index} />
+      <button className="directionBtn" onClick={nextSlide}>
+        &gt;
+      </button>
+    </div>
+  );
+};
+
+export default Slider;
